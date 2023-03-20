1ec93f6d6804668e67d70ecbdf1c977aa6e6dc0e
GALILEI IT <galilej@gmail.com>
2023-03-20 10:13:32 +0100

    Citanje i upisivanje u tekst
    Signed-off-by: GALILEI IT <galilej@gmail.com>

--- index.php
+++ index.php
@@ -1,23 +1,32 @@
 <!DOCTYPE html>
 <html lang="en">
-    <head>
-        <title>Demo</title>
-        <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <link href="css/style.css" rel="stylesheet">
-    </head>
-    <body>
-    <h1>
+
+<head>
+    <title>Demo</title>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link href="css/style.css" rel="stylesheet">
+</head>
+
+<body>
+    <h3>
         <?php
-        $a=5;
-        $b="World";
-            echo "Hello," . " " . $b . "!" . " " . $a;     
+        $myfile = fopen("test.txt", "w+");
+
+        $txt = "Spisak \n -prvi \n -drugi \n -treci";
+        fwrite($myfile, $txt);
+        fclose($myfile);
         ?>
-    </h1>
-    <h2>
+    </h3>
+    
+    <h4>
         <?php
-            echo  nl2br("kings \n garden1 \n garden2"); // nl2br() is used to add a new line
+        $myfile = fopen("test.txt", "r");
+        echo fread($myfile, filesize("test.txt"));
+        fclose($myfile);
         ?>
-    </h2>
-    </body>
+    </h4>
+
+</body>
+
 </html>
