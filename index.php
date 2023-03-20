44324f395a6353d8eb30c33aadb19a89634b274c
GALILEI IT <galilej@gmail.com>
2023-03-20 08:53:02 +0100

    First PHP code
    Signed-off-by: GALILEI IT <galilej@gmail.com>

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <title>Demo</title>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link href="css/style.css" rel="stylesheet">
+    </head>
+    <body>
+    <h1>
+        <?php
+            echo "Hello World"; 
+            // print "test";
+        ?>
+    </h1>
+    </body>
+</html>
